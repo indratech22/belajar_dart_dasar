39392521021ed995b2f3e5df99bbc64b52728d15
Indra Wijaya S <Wijayaindra555@gmail.com>
2022-12-28 18:00:41 +0700

    belajar operator aritmatika dart

---
+++ bin/operator_aritmatika.dart
@@ -0,0 +1,19 @@
+void main () {
+  var hasil1 = 10 + 10;
+  var hasil2 = 100 - 10;
+  var hasil3 = -5;
+  var hasil4 = 5;
+  var hasil5 = 5 * 5;
+  var hasil6 = 10 / 3;
+  var hasil7 = 10 ~/ 3;
+  var hasil8 = 10 % 3;
+
+  print(hasil1);
+  print(hasil2);
+  print(hasil3);
+  print(hasil4);
+  print(hasil5);
+  print(hasil6);
+  print(hasil7);
+  print(hasil8);
+}
