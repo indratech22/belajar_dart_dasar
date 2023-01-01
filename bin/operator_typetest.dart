c74670563e44c40865d2aee9d457abd00651768f
Indra Wijaya S <Wijayaindra555@gmail.com>
2023-01-01 13:14:17 +0700

    belajar operator tyoetest

---
+++ bin/operator_typetest.dart
@@ -0,0 +1,14 @@
+void main (){
+
+  dynamic variable = 100;
+
+  //var variableString = variable as String;// error
+
+  var variableInt = variable as int;
+
+  print(variable);
+  print(variableInt);
+  print(variable is int);
+  print(variable is String);
+  print(variable is bool);
+}
