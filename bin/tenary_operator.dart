d0cd79730eb91ae9a02b6c6311051ee0b10a1c75
Indra Wijaya S <Wijayaindra555@gmail.com>
2023-01-02 12:26:02 +0700

    belajar tenary operator

---
+++ bin/tenary_operator.dart
@@ -0,0 +1,13 @@
+void main (){
+
+  var nilai = 60;
+  String keluar = nilai >=75 ? 'anda lulus':'anda tidak lulus';
+
+  //if(nilai>=75){
+   // keluar = 'anda lulus';
+ // }else{
+  //  keluar = 'anda tidak lulus';
+ // }
+
+  print(keluar);
+}
