c12ab9e8c0bf47c1a4d77a7ee3d1cee4c2bf7b59
Indra Wijaya S <Wijayaindra555@gmail.com>
2023-01-05 18:59:37 +0700

    belajar fungsi kondisi if

---
+++ bin/if.dart
@@ -0,0 +1,11 @@
+void main (){
+
+  var nilaiTugas = 50;
+  var nilaiAbsen = 75;
+
+  if(nilaiAbsen >= 75 && nilaiTugas >= 75){
+  print('anda lulus');
+  } else {
+    print('anda tidak lulus');
+  }
+}
