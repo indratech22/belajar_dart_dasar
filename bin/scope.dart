8b295253a5c0a938fd51c38952a281545c384d87
Indra Wijaya S <Wijayaindra555@gmail.com>
2023-01-16 13:37:21 +0700

    belajar scope{

---
+++ bin/scope.dart
@@ -0,0 +1,13 @@
+void sayHello() {
+  var name = 'indra wijaya';
+  print('hello $name');
+  print(name);
+}
+void main(){
+  print('belajar scope');
+
+  sayHello();
+  // print(name); error
+  }
+
+// print(name); error
