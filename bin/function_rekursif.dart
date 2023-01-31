56216603a7cfca161c4882da312b4d8ff0711b19
Indra Wijaya S <Wijayaindra555@gmail.com>
2023-01-31 18:45:20 +0700

    belajar fungsi rekursif update

---
+++ bin/function_rekursif.dart
@@ -0,0 +1,31 @@
+int factorialLoop (int value){
+  var result = 1;
+
+  for(var i =1; i <= value; i++){
+    result *=i;
+  }
+  return result;
+}
+
+int factorialRecursive (int value){
+  if (value ==1) {
+    return 1;
+  }else{
+    return value*factorialRecursive(value-1);
+  }
+}
+
+void loop (int value){
+  if (value ==0){
+    print('Selesai bos');
+  }else{
+    print('perulangan ke $value');
+    loop(value-1);
+  }
+}
+
+void main(){
+  print(factorialLoop(10));
+  print(factorialRecursive(10));
+  loop(100000);
+}
