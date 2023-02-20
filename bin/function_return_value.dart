0c896eec6b4def58f9e6b47bd3dc7876ec672cb6
Indra Wijaya S <Wijayaindra555@gmail.com>
2023-02-20 18:50:42 +0700

    belajar function return value

---
+++ bin/function_return_value.dart
@@ -0,0 +1,15 @@
+int sum (List<int> numbers){
+  var  total = 0;
+
+  for(var value in numbers){
+    total += value;
+  }
+  return total;
+}
+
+void main (){
+
+  print(sum([10,10,10]));
+  print(sum([20,20,20]));
+
+}
